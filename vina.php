622f31bc7d2b09c09406f0f6dd5389046086e522
Alvinavina05 <alvinasugiarti@gmail.com>
2023-08-31 10:55:32 +0700

    Assign

---
+++ vina.php
@@ -0,0 +1,10 @@
+<?php
+//Menampilkan data diri
+echo "Nama: Alvina Sugiarti
+      NIM: E41220499"
+//Tugas Penjumlahan Srderhana
+$c = 5;
+$b = 4;
+$hasil = $c + $b;
+echo "Hasil dari $c + $b = $hasil";
+?>
